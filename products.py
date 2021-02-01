92ae4fb2f024aef579b50553ed1fc05eeda54522
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-02-02 01:04:37 +0800

    add write data

--- products.py
+++ products.py
@@ -1,4 +1,3 @@
-
 products = []
 while True:
 	name = input('請輸入商品名稱: ')
@@ -10,3 +9,7 @@ print(products)
 
 for p in products:
 	print(p[0], "的價格是", p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
