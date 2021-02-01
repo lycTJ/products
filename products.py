d4b8f1db03f1e67f073115cef42fea2dfc7007fd
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-02-02 01:40:44 +0800

    add read writed csv

--- products.py
+++ products.py
@@ -1,3 +1,14 @@
+#讀取檔案
+
+products = []
+with open('products.csv', 'r') as f:
+	for line in f:
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+
+
 products = []
 while True:
 	name = input('請輸入商品名稱: ')
@@ -10,6 +21,7 @@ print(products)
 for p in products:
 	print(p[0], "的價格是", p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品名稱, 商品價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
