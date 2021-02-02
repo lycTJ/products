53c6b01c8d4f024d4849445d65bffc4e0e5b355d
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-02-03 01:04:01 +0800

    refactor

--- products.py
+++ products.py
@@ -1,37 +1,51 @@
 import os   # operating system
 
+
  #讀取檔案
-products = []
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-	print('找到檔案!')
-	with open('products.csv', 'r', encoding = 'utf-8') as f: 
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding = 'utf-8') as f: 
 		for line in f:
 			if '商品名稱,商品價格' in line:
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-else:
-	print('檔案不存在!')
-
-
+	return products	
 
 # 讓使用者輸入
-products = []
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		products.append([name, price])
+	print(products)
+	return products
 
 # 印出所有購買紀錄
-for p in products:
-	print(p[0], "的價格是", p[1])
+def print_product(products):
+	for p in products:
+		print(p[0], "的價格是", p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品名稱,商品價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('商品名稱,商品價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('找到檔案!')
+		products = read_file(filename)
+	else:
+		print('檔案不存在!')
+
+	products = user_input(products)
+	print_product(products)
+	write_file('products.csv', products)
+
+
+main()
